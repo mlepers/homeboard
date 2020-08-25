67faf62a548440c805699e350ea880524deaa769
Marie Lepers <mv.lepers@gmail.com>
2020-08-25 12:30:23 +0200

    sign in done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,21 @@
 class ProfilesController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        authorize @user
+      end
+    
+      def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render :show
+        authorize @user
+      end
+    
+      private 
+    
+      def user_params
+          params.require(:user).permit(:description)
+      end
+
 end
