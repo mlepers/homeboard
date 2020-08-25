ddf4493aebe742fc80f2193eb351f2d73434e517
Marie Lepers <mv.lepers@gmail.com>
2020-08-25 11:15:59 +0200

    finish the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_082653) do
+ActiveRecord::Schema.define(version: 2020_08_25_091458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,15 @@ ActiveRecord::Schema.define(version: 2020_08_25_082653) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "pseudo"
+    t.bigint "residence_id"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["residence_id"], name: "index_users_on_residence_id"
   end
 
+  add_foreign_key "users", "residences"
 end
