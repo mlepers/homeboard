cf195fa013eb0e85a157c455a7af83e42dc12f32
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-26 21:43:15 +0200

    Date picker ok

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many_attached :photos
+  validate :end_date_after_start_date
 
   include PgSearch::Model
   pg_search_scope :search_by_title_and_description,
@@ -10,4 +11,14 @@ class Service < ApplicationRecord
       tsearch: { prefix: true }
     }
 
+  private
+
+  def end_date_after_start_date
+    unless start_at.nil?
+      if end_at < start_at
+        errors.add(:end_at, "must be after the start date")
+      end
+    end
+  end
+  
 end
