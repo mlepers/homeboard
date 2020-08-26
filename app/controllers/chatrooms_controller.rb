4a22325d6781185d8b23a2a2937cfcb27ddd6794
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-26 14:01:14 +0200

    index chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,9 @@
 class ChatroomsController < ApplicationController
 
+  def index
+    @chatrooms = policy_scope(Chatroom).where('guest_id = ? OR host_id = ?', current_user, current_user)
+  end
+
   def show 
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
