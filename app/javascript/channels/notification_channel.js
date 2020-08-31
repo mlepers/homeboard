cf2e22320c8cbc294b18b34745cfcb9292ce9b6a
Marie Lepers <mv.lepers@gmail.com>
2020-08-31 16:59:06 +0200

    notif from 0 done

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -4,17 +4,17 @@ const initNotificationCable = () => {
  
      consumer.subscriptions.create({ channel: "NotificationChannel" }, {
       received(data) {
-      
         const notificationsContainer = document.querySelector(`#${data.notif_id}`);
-
-        const numberOfNotifications = parseInt(notificationsContainer.innerText);
-
-        const newNotificationBadge = `<div class="avatar-red" id="${data.notif_id}">${numberOfNotifications +1}</div>`;
-
-        notificationsContainer.insertAdjacentHTML('beforebegin', newNotificationBadge);
-        
-        notificationsContainer.remove()
-        
+        if (notificationsContainer == null) {
+          const noNotif = document.querySelector(`#number_of_notif`);
+          const fisrtNotificationBadge = `<div class="avatar-red" id="${data.notif_id}">1</div>`;
+          noNotif.insertAdjacentHTML('beforebegin', fisrtNotificationBadge);  
+        } else {
+          const numberOfNotifications = parseInt(notificationsContainer.innerText);
+          const newNotificationBadge = `<div class="avatar-red" id="${data.notif_id}">${numberOfNotifications +1}</div>`;
+          notificationsContainer.insertAdjacentHTML('beforebegin', newNotificationBadge);  
+          notificationsContainer.remove()
+        }
       },
     });
 }
