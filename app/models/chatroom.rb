aa72c4fd565e1d43368db0b73a22cae835aa0679
Marie Lepers <mv.lepers@gmail.com>
2020-08-31 12:27:44 +0200

    add the function to mark as seen messages when you go through the show

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -7,19 +7,22 @@ class Chatroom < ApplicationRecord
     return [self.guest, self.host]
   end
 
-  def nb_of_unseen_messages
+  def nb_of_unseen_messages(current_user)
     n = 0 
     self.messages.each do |message|
-      if message.seen == false
+      if message.seen == false && message.user_id != current_user.id
         n+=1
       end
     end
     return n
   end
 
-  def mark_all_messages_as_seen
+  def mark_all_messages_recieved_as_seen(current_user)
     self.messages.each do |message|
-      message.seen!
+      unless message.user_id == current_user.id
+        message.seen!
+        message.save
+      end
     end
   end
 
