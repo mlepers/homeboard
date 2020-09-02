52bc8aa750e55d75b46bab324ee43f168f6ee305
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-09-02 13:54:43 +0200

    Mergin

--- app/models/info_syndic.rb
+++ app/models/info_syndic.rb
@@ -2,6 +2,7 @@ class InfoSyndic < ApplicationRecord
   validates :category, inclusion: { in: ["alert", "info"] } 
   belongs_to :residence
   belongs_to :user
+  has_one_attached :file
 
   def seen!
     self.seen = true
