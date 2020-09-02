8b4177256182a7343e601a8ee10bf159b05b7859
Thomas Quetu <quetu.thomas@gmail.com>
2020-09-02 17:33:26 +0200

    ok

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -4,8 +4,8 @@ class ResidencesController < ApplicationController
 
     def show
         @info_syndics = current_user.info_syndics.order("info_syndics.created_at DESC")
-        if (@condition = current_user.charges.first) && (@condition.orders.first.state == 'pending')
-            @charge = Charge.find(current_user.charges.first.id)
+        if (@condition = current_user.charges.first)
+          @charge = Charge.find(current_user.charges.first.id)
         end
         authorize @residence
         @title = "Copro"
