13d99cec3618fc12cbde3203d127d92f6b1f5c6e
Thomas Quetu <quetu.thomas@gmail.com>
2020-09-03 17:12:31 +0200

    debug scrollbar ios

--- app/javascript/plugins/topmenu.js
+++ app/javascript/plugins/topmenu.js
@@ -4,6 +4,16 @@ const topMenu = () => {
   if (header){
     let lastScrollValue = 0;
     document.addEventListener('scroll',() => {
+        console.log(window.scrollY);
+
+
+
+        if (window.scrollY < 100) {
+          return;
+        }
+
+
+
         let top  = document.documentElement.scrollTop;
         if(lastScrollValue < top) {
           header.classList.add("hidden");
